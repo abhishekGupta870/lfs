a027ddbc41f842753566153be933b94ba929722c
abhishekGupta870 <49617223+abhishekGupta870@users.noreply.github.com>
2021-05-09 22:05:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 #from tensorflow import keras
 st.set_page_config(page_title="Plant Disease Detection App", page_icon="icon.png", layout='centered', initial_sidebar_state='auto')
-@st.cache(allow_output_mutation=True,max_entries=10,ttl=3600)
+@st.cache(allow_output_mutation=True)
 def lModel(y):
   x=load_model(y)
   return x
 Xception_model=lModel('Xception.h5')
-#MobileNetV2_model=lModel('MobileNetV2_50_epoch.h5')
-#InceptionV3_model=lModel('InceptionV3.h5')
-#VGG16_model=lModel('VGG16.h5')
-#ResNet50_model= lModel("ResNet50.h5")
-#@st.cache
+MobileNetV2_model=lModel('MobileNetV2_50_epoch.h5')
+InceptionV3_model=lModel('InceptionV3.h5')
+VGG16_model=lModel('VGG16.h5')
+ResNet50_model= lModel("ResNet50.h5")
+@st.cache
 class plant_diseases_detection():
   global pred
   def page_setup():
